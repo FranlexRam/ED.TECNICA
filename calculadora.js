3f2632a48fcc5c9dc7af857d59a24bb5968e6004
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-10-25 21:05:47 -0400

    Corrijiendo calculadora

--- calculadora.js
+++ calculadora.js
@@ -3,16 +3,16 @@ var num2;
 var signo;
 
 function calculadora (x,y,z){
-    if (z="+"){
+    if (z=="+"){
         return x + y;
-    } else if (z="-"){
+    } else if (z=="-"){
         return x - y;
-    } else if (z="*"){
+    } else if (z=="*"){
         return x * y;
-    } else if (signo="/"){
+    } else if (z=="/"){
         return x / y;
     } else {
-        console.log("No conozco ese signo");//Aqui colocar "return" en lugar de un "console.log"
+        console.log("No conozco ese signo");//Aqui colocar "return" en lugar de un "console.log" para ver si arroja resultado
     }
 }
 var resultado = calculadora (num1,num2,signo);

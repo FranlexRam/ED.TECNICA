d3a1f8552f0406cf102d13a742b0324dcb182914
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-10-26 21:50:03 -0400

    Modificando

--- PAGO.js
+++ PAGO.js
@@ -2,16 +2,16 @@
 
 var TD= "Tarjeta de debito";
 var TC="Tarjeta de credito";
+var metPos= "Metodo positivo"
 
 
 var metodo = function (x) {
     if (x==TD){
         console.log("Ingresar datos de " + TD);
-
     } else if (x==TC){
         console.log("Ingresar datos de " + TC);
     } else {
         console.log("Metodo no encontrado");
     }
     }
-metodo ("Tarjeta de debito");
+metodo (TD);

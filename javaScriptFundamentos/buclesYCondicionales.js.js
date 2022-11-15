5a24c3743d0354494c54581df70881f33a1ae90e
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-11-14 21:20:19 -0400

    Learning new methods

--- javaScriptFundamentos/buclesYCondicionales.js.js
+++ javaScriptFundamentos/buclesYCondicionales.js.js
@@ -1,26 +1,25 @@
-const operacion =prompt('Operacion:');
-location.reload;
+const operacion = prompt('Operacion:');
 
 const calculo = () => {
-if (operacion.includes('+')) {
-    const [num1, num2] = operacion.split('+');
-    alert( Number(num1) + Number(num2));
-       
-} else if (operacion.includes('-')) {
-    const [num1,num2] = operacion.split('-');
-    alert( Number(num1) - Number(num2));
-       
-} else if (operacion.includes('*')) {
-    const [num1,num2]=operacion.split('*');
-    alert( Number(num1) * Number(num2));
-        
-} else if (operacion.includes('/')) {
-    const [num1,num2]=operacion.split('/');
-    alert( Number(num1) / Number(num2) );
-      
-} else {
-    alert ('Datos invalidos, ingresa solo valores numericos');
-}
+    if (operacion.includes('+')) {
+        const [num1, num2] = operacion.split('+');
+        alert(Number(num1) + Number(num2));
+
+    } else if (operacion.includes('-')) {
+        const [num1, num2] = operacion.split('-');
+        alert(Number(num1) - Number(num2));
+
+    } else if (operacion.includes('*')) {
+        const [num1, num2] = operacion.split('*');
+        alert(Number(num1) * Number(num2));
+
+    } else if (operacion.includes('/')) {
+        const [num1, num2] = operacion.split('/');
+        alert(Number(num1) / Number(num2));
+
+    } else {
+        alert('Datos invalidos, ingresa solo valores numericos');
+    }
 
 }
 

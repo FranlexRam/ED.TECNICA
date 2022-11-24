29887bb7303f2af1589cd60bed1df65d36e3de44
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-11-24 09:51:20 -0400

    CRUD lista contactos

---
+++ listaDeContactos/contactos.js
@@ -0,0 +1,91 @@
+var selectedRow = null;
+
+//show alerts
+function showAlert(message, className){
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const main = document.querySelector('.main');
+    container.insertBefore(div, main);
+
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+}   
+
+//Clear all fields
+
+function clearFields() {
+    document.querySelector('#firstName').value = '';
+    document.querySelector('#lastName').value = '';
+    document.querySelector('#rollNo').value = '';    
+}
+
+
+//Add data
+
+document.querySelector('#student-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Get form values
+    const firstName = document.querySelector('#firstName').value;
+    const lastName = document.querySelector('#lastName').value;
+    const rollNo = document.querySelector('#rollNo').value;
+
+    //validate
+    if (firstName == '' || lastName == '' || rollNo == '') {
+        showAlert('Por favor llena todos los campos.', 'danger');        
+    } else {
+        if (selectedRow == null) {
+            const list = document.querySelector('#student-list');
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${rollNo}</td>
+            <td>
+            <a href="#" class="btn btn-warning bt-sm edit">Editar</a>
+            <a href="#" class="btn btn-danger bt-sm delete">Borrar</a>
+            </td>            
+            `;
+            list.appendChild(row);
+            selectedRow = null;
+            showAlert('Nuevo contacto agregado.', "success");            
+        } else {
+            selectedRow.children[0].textContent = firstName;
+            selectedRow.children[1].textContent = lastName;
+            selectedRow.children[2].textContent = rollNo;
+            selectedRow = null;
+            showAlert('Informacion de contacto actualizada.', 'info');
+        }
+
+        clearFields();
+
+
+    }
+});
+
+//Edit data
+
+document.querySelector('#student-list').addEventListener('click', (e) => {
+    target = e.target;
+    if(target.classList.contains('edit')){
+        selectedRow = target.parentElement.parentElement;
+        document.querySelector('#firstName').value = selectedRow.children[0].textContent;
+        document.querySelector('#lastName').value = selectedRow.children[1].textContent;
+        document.querySelector('#rollNo').value = selectedRow.children[2].textContent;
+    }
+})
+
+
+//Delete data
+
+document.querySelector('#student-list').addEventListener('click', (e) =>{
+    target = e.target;
+
+    if (target.classList.contains('delete')) {
+        target.parentElement.parentElement.remove();
+        showAlert('Contacto borrado.', 'danger');        
+    }
+});

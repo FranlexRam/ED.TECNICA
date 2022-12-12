11f88aea79cb24e5f111af6967f0f44696425e35
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-12-12 19:04:26 -0400

    logica arbolito

--- EJERCICIOS-LOGICA/ejercicio1.js
+++ EJERCICIOS-LOGICA/ejercicio1.js
@@ -9,26 +9,26 @@
 // //   arbol(15);
 
 
-// //SOLUCION #2:
-// function arbol(numPisos) {
-//     let i = 0;
-//     while (i < numPisos) {
-//         let asterisco = '*';
-//         console.log('.'.repeat(numPisos - i), asterisco.repeat(i), asterisco, asterisco.repeat(i));
-//         i++;  
-//     }
-// }
-// arbol(6);
+//SOLUCION #2:
+function arbol(numPisos) {
+    let i = 0;
+    while (i < numPisos) {
+        let asterisco = '*';
+        console.log('.'.repeat(numPisos - i), asterisco.repeat(i), asterisco, asterisco.repeat(i));
+        i++;  
+    }
+}
+arbol(6);
 
 
 
 // PRACTICANDO:
 
-function arbol(num) {
-    for (let i = 0; i < num; i++) {
-        let asterisco = '*';
-        console.log(' '.repeat(10 + num - i), asterisco.repeat(i), asterisco, asterisco.repeat(i));
+// function arbol(num) {
+//     for (let i = 0; i < num; i++) {
+//         let asterisco = '*';
+//         console.log(' '.repeat(10 + num - i), asterisco.repeat(i), asterisco, asterisco.repeat(i));
         
-    }
-}
-arbol(31);
+//     }
+// }
+// arbol(31);
